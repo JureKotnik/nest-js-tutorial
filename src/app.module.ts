c84befe2320ed71a4eb64155d26072596a668962
JureKotnik <jure2002k@gmail.com>
2023-04-20 14:46:59 +0200

    Installing PostgreSQL

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common';
 import { BoardsModule } from './boards/boards.module';
 import { BoardsController } from './boards/boards.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { typeORMConfig } from './configs/typeorm.config';
 
 
 @Module({
-  imports: [BoardsModule],
+  
+  imports: [
+    TypeOrmModule.forRoot(typeORMConfig),
+    BoardsModule
+  ],
+  
   controllers: [BoardsController],
 
 })
--- src/boards/boards.module.ts
+++ src/boards/boards.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { BoardsController } from './boards.controller';
 import { BoardsService } from './boards.service';
+import { BoardRepository } from './board.repository';
 
 
 @Module({
+    imports:[
+        TypeOrmModule.forFeature([BoardRepository])
+    ],
     controllers: [ BoardsController ],
     providers: [BoardsService]
 })
