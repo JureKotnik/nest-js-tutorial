56abcb1a5e29f476b3fdb847379d742eb81d8126
JureKotnik <jure2002k@gmail.com>
2023-04-20 12:41:36 +0200

    Create a service to get all posts

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,4 +1,41 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, Delete, Patch } from '@nestjs/common';
+import { BoardsService } from './boards.service';
+import { Board, BoardStatus } from './board.model';
+import { CreateBoardDto } from './dto/create-board.dto';
 
 @Controller('boards')
-export class BoardsController {}
+export class BoardsController {
+
+    constructor(private boardsService: BoardsService){}
+
+    @Get()
+    getAllBoards(): Board[]{
+        return this.boardsService.getAllBoards();
+    }
+
+    @Post()
+    createBoard(
+        @Body() createBoardDto: CreateBoardDto)
+     :Board{
+        return this.boardsService.createBoard(createBoardDto);
+    }
+
+    @Get('/:id')
+    getBoardById(@Param('id') id: string): Board{
+        return this. boardsService.getBoardById(id);
+    }
+
+    @Delete('/:id')
+    deleteBoard(@Param('id') id: string): void {
+        this.boardsService.deleteBoard(id);
+    }
+
+    @Patch('/:id/status')
+    updateBoardStatus(
+        @Param('id') id: string,
+        @Body('status') status: BoardStatus,
+    ){
+        return this.boardsService.updateBoardStatus(id, status);
+    }
+
+}
